4d8d78c4bb6c0cdd411014482944dacadf33cf5b
Christian <chweber@mit.edu>
2024-05-13 09:56:04 -0400

    Modify 'multi_similarity_loss.py' for my purposes

--- BrainEncoder/multi_similarity_loss.py
+++ BrainEncoder/multi_similarity_loss.py
@@ -8,34 +8,36 @@
 import torch
 from torch import nn
 
-from ret_benchmark.losses.registry import LOSS
-
-
-@LOSS.register('ms_loss')
 class MultiSimilarityLoss(nn.Module):
-    def __init__(self, cfg):
+    def __init__(self):#, cfg):
         super(MultiSimilarityLoss, self).__init__()
         self.thresh = 0.5
         self.margin = 0.1
 
-        self.scale_pos = cfg.LOSSES.MULTI_SIMILARITY_LOSS.SCALE_POS
-        self.scale_neg = cfg.LOSSES.MULTI_SIMILARITY_LOSS.SCALE_NEG
+        self.scale_pos = 2.0  # cfg.LOSSES.MULTI_SIMILARITY_LOSS.SCALE_POS
+        self.scale_neg = 40.0 # cfg.LOSSES.MULTI_SIMILARITY_LOSS.SCALE_NEG
 
-    def forward(self, feats, labels):
-        assert feats.size(0) == labels.size(0), \
-            f"feats.size(0): {feats.size(0)} is not equal to labels.size(0): {labels.size(0)}"
-        batch_size = feats.size(0)
-        sim_mat = torch.matmul(feats, torch.t(feats))
+    def forward(self, features, labels):
+        assert features.size(0) == labels.size(0), \
+            f"features.size(0): {features.size(0)} is not equal to labels.size(0): {labels.size(0)}"
+        batch_size = features.size(0)
+        sim_mat = torch.matmul(features, torch.t(features))
 
         epsilon = 1e-5
         loss = list()
 
         for i in range(batch_size):
+            if i > 0: break
             pos_pair_ = sim_mat[i][labels == labels[i]]
             pos_pair_ = pos_pair_[pos_pair_ < 1 - epsilon]
+
+            if len(pos_pair_) < 1: continue
+
             neg_pair_ = sim_mat[i][labels != labels[i]]
 
             neg_pair = neg_pair_[neg_pair_ + self.margin > min(pos_pair_)]
+
+            if len(neg_pair_) < 1: continue
             pos_pair = pos_pair_[pos_pair_ - self.margin < max(neg_pair_)]
 
             if len(neg_pair) < 1 or len(pos_pair) < 1:
